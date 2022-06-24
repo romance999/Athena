61d2f0270b6beb3d290b94e2d0af4d1b955d8f88
romance <66000596+romance999@users.noreply.github.com>
2022-06-24 17:26:26 -0400

    Create Data.cs

---
+++ Athena/Handler/Data.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Athena.Handler
+{
+    class Data
+    {
+        public static bool antiafk;
+        public static bool spam;
+    }
+}
