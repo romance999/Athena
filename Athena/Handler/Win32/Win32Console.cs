c8abe17b9b5f69c00f3e3230d4834cd7e150884e
romance <66000596+romance999@users.noreply.github.com>
2022-06-24 17:26:34 -0400

    Add files via upload

---
+++ Athena/Handler/Win32/Win32Console.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Athena.Handler.Win32
+{
+    class Win32Console
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+
+        public static extern bool AllocConsole();  //allocate console
+
+        public static void Update(string message)
+        {
+            Console.Title = "Athena Console";
+            Console.WriteLine(message);
+        }
+    }
+}
